8511e2f10b6d7a09d11ac8d387aa03017839cf25
tiara2187 <tiaramuttianingtyas23@gmail.com>
2021-05-09 13:00:41 +0700

    perubahan di bcryptjs di ts

--- controllers/UserController.ts
+++ controllers/UserController.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { User } from '../models/User'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 
 class UserController {
